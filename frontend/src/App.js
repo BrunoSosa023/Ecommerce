5179efa3121eee7b282f8edaa19d3fadd5f7a7c3
BrunoSosa023 <107007959+BrunoSosa023@users.noreply.github.com>
2022-11-26 17:45:33 -0600

    Create React App

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Mateando el Tiempo</a>
+      </header>
+      <main>list product</main>
+    </div>
+  );
+}
+
+export default App;
