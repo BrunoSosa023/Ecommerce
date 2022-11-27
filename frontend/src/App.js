af32839f8e047a0815f8ff38598b4dd48e2ceacb
BrunoSosa023 <107007959+BrunoSosa023@users.noreply.github.com>
2022-11-26 18:21:35 -0600

    list products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,30 @@
+import data from './data';
+
 function App() {
   return (
     <div>
       <header>
         <a href="/">Mateando el Tiempo</a>
       </header>
-      <main>list product</main>
+      <main>
+        <h1>Featured products</h1>
+        <div className="products"> 
+        {data.products.map((product) => (
+          <div className="product" key={product.slug}>
+            <a href={`/product/${product.slug}`}>
+            <img src={product.image} alt={product.name} />
+            </a>
+            <div className="product-info">
+            <a href={`/product/${product.slug}`}>
+              <p>{product.name}</p>
+            </a>
+              <p><strong>${product.price}</strong></p>
+              <button>Add to cart</button>
+            </div>
+          </div>
+        ))}
+        </div>
+      </main>
     </div>
   );
 }
