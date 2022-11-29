759a7d02565c7f25bb1c9c43415780729d078249
ArianOC <ivomariane1@gmail.com>
2022-11-28 22:47:22 -0300

    errMessage and messageBox

---
+++ frontend/src/components/MessageBox.js
@@ -0,0 +1,8 @@
+import Alert from "react-bootstrap/Alert";
+import React from "react";
+
+function MessageBox(props) {
+  return <Alert variant={props.variant || "info"}>{props.children}</Alert>;
+}
+
+export default MessageBox;
