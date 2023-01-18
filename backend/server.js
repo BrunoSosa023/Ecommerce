2c4a8a5d4e4ddbf8d658838e322b373e9cde04c5
ArianOC <ivomariane1@gmail.com>
2023-01-18 18:10:47 -0300

    Create Signin Backend API

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,35 @@
-import express from 'express';
-import data from './data.js';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import seedRouter from './routes/seedRoutes.js';
-import productRouter from './routes/productRoutes.js';
+import express from "express";
+import data from "./data.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import seedRouter from "./routes/seedRoutes.js";
+import productRouter from "./routes/productRoutes.js";
+import userRouter from "./routes/userRoutes.js";
 
-mongoose.set('strictQuery', false);
+mongoose.set("strictQuery", false);
 dotenv.config();
 
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
-    console.log('connected to db');
+    console.log("connected to db");
   })
   .catch((err) => {
     console.log(err.message);
   });
 
 const app = express();
-app.use('/api/seed', seedRouter);
-app.use('/api/products', productRouter);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/seed", seedRouter);
+app.use("/api/products", productRouter);
+app.use("/api/users", userRouter);
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
 
 const port = process.env.PORT || 3002;
 app.listen(port, () => {
